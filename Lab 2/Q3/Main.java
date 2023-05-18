f02a190a970cb228273f3bd59d5a61c0dbd9261b
shrxddhx <133136318+shrxddhx@users.noreply.github.com>
2023-05-18 22:23:50 +0530

    q3

---
+++ Lab 2/Q3/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args) {
+	Mother m1 = new Child(); 
+	m1.show();
+	//despite of a mother class type in run time polymorphism 
+	//the refring class object is given more priority 
+	}
+}
